b9f69edfdcc90d5b68d748989e87e8c6690b835d
Yusif Salam-zade <yusalam@gmail.com>
2019-11-19 17:29:31 +0200

    implement Cell object

--- GameOf2048/src/main/java/gameof2048/domain/Cell.java
+++ GameOf2048/src/main/java/gameof2048/domain/Cell.java
@@ -1,4 +1,26 @@
 package gameof2048.domain;
 
 public class Cell {
+    private int value;
+
+    public Cell(){
+        this.value = 0;
+    }
+
+    public Cell(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value+"";
+    }
 }
