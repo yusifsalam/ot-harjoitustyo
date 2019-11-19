2a072096a2af2c19ad6011d4d3a214d2f626e5cf
Yusif Salam-zade <yusalam@gmail.com>
2019-11-19 15:04:11 +0200

    start screen ui prototype

--- GameOf2048/src/main/java/gameof2048/ui/GameUI.java
+++ GameOf2048/src/main/java/gameof2048/ui/GameUI.java
@@ -1,12 +1,25 @@
 package gameof2048.ui;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class GameUI extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("2048");
+        VBox startPane = new VBox(10);
+        Label gameTitle = new Label("Game of 2048");
+        Button newGameBtn = new Button("NEW GAME");
+        Button highscoresBtn = new Button("HIGHSCORES");
+        Button myStatsBtn = new Button("MY STATS");
+        startPane.getChildren().addAll(gameTitle, newGameBtn, highscoresBtn, myStatsBtn);
+
+        Scene scene = new Scene(startPane, 600, 400);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
