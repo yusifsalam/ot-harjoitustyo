d4abfc46554d85a69a47ac02fea70500ff9a585e
Yusif Salam-zade <yusalam@gmail.com>
2019-11-19 17:29:50 +0200

    implement user object

--- GameOf2048/src/main/java/gameof2048/domain/User.java
+++ GameOf2048/src/main/java/gameof2048/domain/User.java
@@ -1,4 +1,29 @@
 package gameof2048.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    private String username;
+    private int highscore;
+    private List<Integer> history;
+
+    public User(String username){
+        this.username = username;
+        this.highscore = 0;
+        this.history = new ArrayList<>();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return username;
+    }
 }
