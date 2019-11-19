5d0d34db6dc36968bf10feee885c4a0f90dffd49
Yusif Salam-zade <yusalam@gmail.com>
2019-11-19 17:30:07 +0200

    implement game object

--- GameOf2048/src/main/java/gameof2048/domain/Game.java
+++ GameOf2048/src/main/java/gameof2048/domain/Game.java
@@ -1,4 +1,22 @@
 package gameof2048.domain;
 
 public class Game {
+    private Board board;
+    private User user;
+    private int score;
+    private boolean gameOver;
+
+    public Game(Board board, User user) {
+        this.board = board;
+        this.user = user;
+        this.score = 0;
+        this.gameOver = false;
+    }
+
+    public String toString() {
+        String res = "Username: " + user.toString();
+        res += "\n Score: "+ score;
+        res += "\n Board: \n" + board.toString();
+        return res;
+    }
 }
