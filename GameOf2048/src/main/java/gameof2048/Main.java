e45cb6e9c374a15eb5434e2c745b2f277303c3b8
Yusif Salam-zade <yusalam@gmail.com>
2019-11-19 17:31:12 +0200

    cli base for main class

--- GameOf2048/src/main/java/gameof2048/Main.java
+++ GameOf2048/src/main/java/gameof2048/Main.java
@@ -1,9 +1,55 @@
 package gameof2048;
 
+import gameof2048.domain.Board;
+import gameof2048.domain.Game;
+import gameof2048.domain.User;
 import gameof2048.ui.GameUI;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        GameUI.main(args);
+        User user = new User("Unknown");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Let the game begin");
+
+        int userChoice = 0;
+        while (userChoice != 4) {
+            printActions();
+            userChoice = scanner.nextInt();
+            switch (userChoice) {
+                case 1:
+                    GameUI.main(args);
+                    break;
+                case 2:
+                    System.out.println("What is your name?");
+                    String name = scanner.next();
+                    user.setUsername(name);
+                    break;
+                case 3:
+                    System.out.println("Please pick a random seed");
+                    int seed = scanner.nextInt();
+                    Random r = new Random(seed);
+                    Board board = new Board(r);
+                    Game game = new Game(board, user);
+                    System.out.println(game);
+                    break;
+                case 4:
+                    break;
+                default:
+                    printActions();
+            }
+        }
+
+    }
+
+    public static void printActions() {
+        System.out.println("Available actions:");
+        System.out.println("\t1. Open GUI");
+        System.out.println("\t2. Set your name");
+        System.out.println("\t3. Start game in command line mode");
+        System.out.println("\t4. Quit");
     }
 }
