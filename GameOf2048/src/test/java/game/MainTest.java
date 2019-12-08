21b42668e65af08c14ed9eaaf86c997531518456
Yusif Salam-zade <yusalam@gmail.com>
2019-12-08 21:40:15 +0200

    update tests

--- GameOf2048/src/test/java/game/MainTest.java
+++ GameOf2048/src/test/java/game/MainTest.java
@@ -4,6 +4,7 @@ import game.domain.Board;
 import game.domain.Game;
 import game.domain.User;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Random;
@@ -21,6 +22,7 @@ public class MainTest {
         game = new Game(board, new User("Unknown"), new Scanner(""));
     }
 
+
     @Test
     public void gameIsGeneratedCorrectly() {
         assertEquals(2, game.getBoard().getCellValueAt(0));
@@ -28,12 +30,14 @@ public class MainTest {
         assertEquals("Unknown", game.getUser().getUsername());
     }
 
+
     @Test
     public void movingDownWorksCorrectly() {
         game.moveBoardDown();
         assertEquals(2, game.getBoard().getCellValueAt(4));
         assertEquals(4, game.getBoard().getCellValueAt(15));
     }
+
 
     @Test
     public void movingUpWorksCorrectly() {
@@ -56,6 +60,68 @@ public class MainTest {
         game.moveBoardLeft();
         assertEquals(2, game.getBoard().getCellValueAt(0));
         assertEquals(4, game.getBoard().getCellValueAt(10));
+    }
+
+    @Test
+    public void gameCanBeWon() {
+        game.getBoard().setCellValueAt(2, 1024);
+        game.getBoard().setCellValueAt(3, 1024);
+        System.out.println(game);
+        assertFalse(game.isGameOver());
+        game.moveBoardRight();
+        System.out.println(game);
+        assertTrue(game.gameWon());
+    }
+
+    @Test
+    public void gameCanBeLost() {
+        game.getBoard().setCellValueAt(0,4);
+        game.getBoard().setCellValueAt(1,2);
+        game.getBoard().setCellValueAt(2,4);
+        game.getBoard().setCellValueAt(3,8);
+
+        game.getBoard().setCellValueAt(4,2);
+        game.getBoard().setCellValueAt(5,4);
+        game.getBoard().setCellValueAt(6,8);
+        game.getBoard().setCellValueAt(7,128);
+
+        game.getBoard().setCellValueAt(8,4);
+        game.getBoard().setCellValueAt(9,8);
+        game.getBoard().setCellValueAt(10,32);
+        game.getBoard().setCellValueAt(11,256);
+
+        game.getBoard().setCellValueAt(12,8);
+        game.getBoard().setCellValueAt(13,16);
+        game.getBoard().setCellValueAt(14,2);
+        game.getBoard().setCellValueAt(15,512);
+        game.moveBoardRight();
+        assertTrue(game.isGameOver());
+
+    }
+
+    @Test
+    public void scoreIsCalculatedCorrectly() {
+        game.getBoard().setCellValueAt(0,4);
+        game.getBoard().setCellValueAt(1,2);
+        game.getBoard().setCellValueAt(2,4);
+        game.getBoard().setCellValueAt(3,8);
+
+        game.getBoard().setCellValueAt(4,2);
+        game.getBoard().setCellValueAt(5,4);
+        game.getBoard().setCellValueAt(6,8);
+        game.getBoard().setCellValueAt(7,128);
+
+        game.getBoard().setCellValueAt(8,4);
+        game.getBoard().setCellValueAt(9,8);
+        game.getBoard().setCellValueAt(10,32);
+        game.getBoard().setCellValueAt(11,256);
+
+        game.getBoard().setCellValueAt(12,8);
+        game.getBoard().setCellValueAt(13,16);
+        game.getBoard().setCellValueAt(14,2);
+        game.getBoard().setCellValueAt(15,512);
+        game.moveBoardRight();
+        assertEquals(998, game.getBoard().calculateScore());
     }
 
     @Test
