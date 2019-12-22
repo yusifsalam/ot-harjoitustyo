ea38207d2f73987b665a5269121cd4b153202c57
Yusif Salam-zade <yusalam@gmail.com>
2019-12-22 12:39:28 +0200

    convert User class to Entity

--- GameOf2048/src/main/java/game/domain/User.java
+++ GameOf2048/src/main/java/game/domain/User.java
@@ -1,11 +1,28 @@
 package game.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String username;
     private int highscore;
+
+    @ElementCollection
     private List<Integer> history;
 
     public User(String username) {
