ea917f434acb6bc6d0ecd0461e419a05d9aaa8bb
Yusif Salam-zade <yusalam@gmail.com>
2019-12-22 14:32:41 +0200

    User service can create new user and update name

--- GameOf2048/src/main/java/game/dao/UserService.java
+++ GameOf2048/src/main/java/game/dao/UserService.java
@@ -1,10 +1,23 @@
 package game.dao;
 
 import game.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
 
-    public void create(String name) {
+    @Autowired
+    private UserRepository ur;
+
+    public User create(String name) {
         User user = new User(name);
+        return ur.save(user);
+    }
+
+    public User updateName(String oldName, String newName) {
+        User user = ur.findByUsername(oldName);
+        user.setUsername(newName);
+        return ur.save(user);
     }
 }
